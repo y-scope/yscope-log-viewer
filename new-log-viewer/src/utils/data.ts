d6943989af5c284f85fd65b3957d3eb1cbd5cfc8
Junhao Liao <junhao@junhao.ca>
2024-08-17 03:10:35 -0400

    Add utility function to get map key by value.

---
+++ new-log-viewer/src/utils/data.ts
@@ -0,0 +1,18 @@
+import {Nullable} from "../typings/common";
+
+
+/**
+ * Finds the key in a map based on the provided value
+ *
+ * @param map
+ * @param value
+ * @return The key if found, null otherwise.
+ */
+const getMapKeyByValue = <T, M>(map: Map<T, M>, value: M): Nullable<T> => {
+    const entry = [...map].find(([, val]) => val === value);
+    return ("undefined" !== typeof entry) ?
+        entry[0] :
+        null;
+};
+
+export {getMapKeyByValue};
