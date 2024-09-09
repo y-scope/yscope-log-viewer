1c9bbec3982f53ebde4eb0cbb8b1e3f3e4f8d47a
Henry8192 <50559854+Henry8192@users.noreply.github.com>
2024-09-09 16:11:07 -0400

    Add helperText to each form fields

--- new-log-viewer/src/components/ConfigDialog.tsx
+++ new-log-viewer/src/components/ConfigDialog.tsx
@@ -1,4 +1,5 @@
 import FormControl from "@mui/joy/FormControl/FormControl";
+import FormHelperText from "@mui/joy/FormHelperText";
 import FormLabel from "@mui/joy/FormLabel/FormLabel";
 import Input from "@mui/joy/Input";
 
@@ -10,30 +11,33 @@ import {getConfig} from "../utils/config";
 
 
 const formFields = [
-    // FIXME: add helper text for each field.
     {
         initialValue: getConfig(CONFIG_KEY.DECODER_OPTIONS).formatString,
         label: LOCAL_STORAGE_KEY.DECODER_OPTIONS_FORMAT_STRING,
         name: LOCAL_STORAGE_KEY.DECODER_OPTIONS_FORMAT_STRING,
         type: "text",
+        helperText: "The format string used to decode the log messages.",
     },
     {
         initialValue: getConfig(CONFIG_KEY.DECODER_OPTIONS).logLevelKey,
         label: LOCAL_STORAGE_KEY.DECODER_OPTIONS_LOG_LEVEL_KEY,
         name: LOCAL_STORAGE_KEY.DECODER_OPTIONS_LOG_LEVEL_KEY,
         type: "text",
+        helperText: "The key used to determine the log level.",
     },
     {
         initialValue: getConfig(CONFIG_KEY.DECODER_OPTIONS).timestampKey,
         label: LOCAL_STORAGE_KEY.DECODER_OPTIONS_TIMESTAMP_KEY,
         name: LOCAL_STORAGE_KEY.DECODER_OPTIONS_TIMESTAMP_KEY,
         type: "text",
+        helperText: "The key used to determine the timestamp.",
     },
     {
         initialValue: getConfig(CONFIG_KEY.PAGE_SIZE),
         label: LOCAL_STORAGE_KEY.PAGE_SIZE,
         name: LOCAL_STORAGE_KEY.PAGE_SIZE,
         type: "number",
+        helperText: "The number of log messages to display per page.",
     },
 ];
 
@@ -54,6 +58,9 @@ const ConfigDialog = () => {
                     <FormLabel>
                         {field.label}
                     </FormLabel>
+                    <FormHelperText>
+                        {field.helperText}
+                    </FormHelperText>
                     <Input
                         slotProps={{
                             input: {
