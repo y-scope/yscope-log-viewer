c328ec21d9d3d800c51aafcebf8e23d7572b8da9
Junhao Liao <junhao@junhao.ca>
2024-09-11 19:59:53 -0400

    Remove ineffective `.status-button` class.

--- new-log-viewer/src/components/StatusBar/index.tsx
+++ new-log-viewer/src/components/StatusBar/index.tsx
@@ -39,7 +39,6 @@ const StatusBar = () => {
                 Status message
             </Typography>
             <Button
-                className={"status-button"}
                 size={"sm"}
                 onClick={handleCopyLinkButtonClick}
             >
