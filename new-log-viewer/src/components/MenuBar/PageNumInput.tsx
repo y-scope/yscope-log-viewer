y-scope/yscope-log-viewer

commit cfa9274b27c9891de390c0674261e0d4bfa4388c

new-log-viewer: Implement MenuBar and StatusBar with JoyUI components; add JoyUI custom app theme. (#59)

new-log-viewer/src/components/MenuBar/PageNumInput.tsx
@@ -1,0 +1,99 @@
+import React, {
+    useContext,
+    useEffect,
+    useRef,
+    useState,
+} from "react";
+
+import {Typography} from "@mui/joy";
+import Input from "@mui/joy/Input";
+
+import {StateContext} from "../../contexts/StateContextProvider";
+
+import "./PageNumInput.css";
+
+
+const PAGE_NUM_INPUT_FIT_EXTRA_WIDTH = 2;
+
+
+/**
+ * Renders a component for inputting page number.
+ *
+ * @return
+ */
+const PageNumInput = () => {
+    const {loadPage, numPages, pageNum} = useContext(StateContext);
+    const adjustedPageNum = (null === pageNum) ?
+        0 :
+        pageNum;
+
+    const [isEditing, setIsEditing] = useState<boolean>(false);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleSubmit = (ev?: React.FormEvent<HTMLFormElement>) => {
+        if ("undefined" !== typeof ev) {
+            ev.preventDefault();
+        }
+        if (null === inputRef.current || false === isEditing) {
+            return;
+        }
+
+        loadPage(Number(inputRef.current.value));
+        setIsEditing(false);
+    };
+
+    const handleBlur = () => {
+        handleSubmit();
+    };
+
+    const handleInputClick = () => {
+        inputRef.current?.select();
+    };
+
+    const adjustInputWidth = () => {
+        if (null === inputRef.current) {
+            return;
+        }
+        inputRef.current.style.width = "0";
+        inputRef.current.style.width = `${inputRef.current.scrollWidth + PAGE_NUM_INPUT_FIT_EXTRA_WIDTH}px`;
+    };
+
+    const handleInputChange = () => {
+        setIsEditing(true);
+        adjustInputWidth();
+    };
+
+    useEffect(() => {
+        if (null === inputRef.current) {
+            return;
+        }
+        inputRef.current.value = adjustedPageNum.toString();
+        adjustInputWidth();
+    }, [adjustedPageNum]);
+
+    return (
+        <form
+            onSubmit={handleSubmit}
+        >
+            <Input
+                className={"page-num-input"}
+                size={"sm"}
+                slotProps={{input: {ref: inputRef}}}
+                type={"number"}
+                endDecorator={
+                    <Typography
+                        className={"page-num-input-num-pages-text"}
+                        level={"body-md"}
+                    >
+                        {"/ "}
+                        {numPages}
+                    </Typography>
+                }
+                onBlur={handleBlur}
+                onChange={handleInputChange}
+                onClick={handleInputClick}/>
+        </form>
+    );
+};
+
+export default PageNumInput;

new-log-viewer/src/utils/config.ts
@@ -41,10 +41,7 @@ const testConfig = ({key, value}: ConfigUpdate): Nullable<string> => {
             }
             break;
         case CONFIG_KEY.THEME:
-            if (false === (Object.values(THEME_NAME)).includes(value)) {
-                result = "Invalid theme name.";
-            }
-            break;
+            throw new Error(`${key} should be handled by JoyUI instead.`);
         case CONFIG_KEY.PAGE_SIZE:
             if (0 >= value || MAX_PAGE_SIZE < value) {
                 result = `Page size must be greater than 0 and less than ${MAX_PAGE_SIZE + 1}.`;
@@ -89,8 +86,7 @@ const setConfig = ({key, value}: ConfigUpdate): Nullable<string> => {
             );
             break;
         case CONFIG_KEY.THEME:
-            window.localStorage.setItem(LOCAL_STORAGE_KEY.THEME, value);
-            break;
+            throw new Error(`${key} should be handled by JoyUI instead.`);
         case CONFIG_KEY.PAGE_SIZE:
             window.localStorage.setItem(LOCAL_STORAGE_KEY.PAGE_SIZE, value.toString());
             break;
@@ -124,10 +120,8 @@ const getConfig = <T extends CONFIG_KEY>(key: T): ConfigMap[T] => {
                 ),
             } as DecoderOptionsType;
             break;
-        case CONFIG_KEY.THEME: {
-            value = window.localStorage.getItem(LOCAL_STORAGE_KEY.THEME);
-            break;
-        }
+        case CONFIG_KEY.THEME:
+            throw new Error(`${key} should be handled by JoyUI instead.`);
         case CONFIG_KEY.PAGE_SIZE:
             value = window.localStorage.getItem(LOCAL_STORAGE_KEY.PAGE_SIZE);
             break;

new-log-viewer/src/App.tsx
@@ -10,13 +10,11 @@ import UrlContextProvider from "./contexts/UrlContextProvider";
  */
 const App = () => {
     return (
-        <>
-            <UrlContextProvider>
-                <StateContextProvider>
-                    <Layout/>
-                </StateContextProvider>
-            </UrlContextProvider>
-        </>
+        <UrlContextProvider>
+            <StateContextProvider>
+                <Layout/>
+            </StateContextProvider>
+        </UrlContextProvider>
     );
 };
 

new-log-viewer/src/components/DropFileContainer/index.css
@@ -1,6 +1,4 @@
 .drop-file-container {
-    width: 100%;
-    height: 100%;
     position: relative;
 }
 

new-log-viewer/src/components/Editor/index.css
@@ -1,0 +1,3 @@
+.editor {
+    height: calc(100vh - var(--ylv-menu-bar-height) - var(--ylv-status-bar-height));
+}

new-log-viewer/src/components/Editor/index.tsx
@@ -16,7 +16,11 @@ import {
 import {Nullable} from "../../typings/common";
 import {CONFIG_KEY} from "../../typings/config";
 import {BeginLineNumToLogEventNumMap} from "../../typings/worker";
-import {EDITOR_ACTIONS} from "../../utils/actions";
+import {
+    ACTION_NAME,
+    EDITOR_ACTIONS,
+    handleAction,
+} from "../../utils/actions";
 import {
     CONFIG_DEFAULT,
     getConfig,
@@ -27,22 +31,18 @@ import {
     getMapValueWithNearestLessThanOrEqualKey,
 } from "../../utils/data";
 import MonacoInstance from "./MonacoInstance";
-import {CustomActionCallback} from "./MonacoInstance/typings";
+import {goToPositionAndCenter} from "./MonacoInstance/utils";
 
+import "./index.css";
 
-interface EditorProps {
-    onCustomAction: CustomActionCallback,
-}
 
 /**
  * Renders a read-only editor for viewing logs.
  *
- * @param props
- * @param props.onCustomAction
  * @return
  */
-const Editor = ({onCustomAction}: EditorProps) => {
-    const {logData, beginLineNumToLogEventNum} = useContext(StateContext);
+const Editor = () => {
+    const {beginLineNumToLogEventNum, logData, numEvents} = useContext(StateContext);
     const {logEventNum} = useContext(UrlContext);
 
     const [lineNum, setLineNum] = useState<number>(1);
@@ -51,7 +51,39 @@ const Editor = ({onCustomAction}: EditorProps) => {
     );
     const editorRef = useRef<Nullable<monaco.editor.IStandaloneCodeEditor>>(null);
     const isMouseDownRef = useRef<boolean>(false);
+    const logEventNumRef = useRef<Nullable<number>>(logEventNum);
+    const numEventsRef = useRef<Nullable<number>>(numEvents);
     const pageSizeRef = useRef(getConfig(CONFIG_KEY.PAGE_SIZE));
+
+    const handleEditorCustomAction = useCallback((
+        editor: monaco.editor.IStandaloneCodeEditor,
+        actionName: ACTION_NAME
+    ) => {
+        if (null === logEventNumRef.current || null === numEventsRef.current) {
+            return;
+        }
+        switch (actionName) {
+            case ACTION_NAME.FIRST_PAGE:
+            case ACTION_NAME.PREV_PAGE:
+            case ACTION_NAME.NEXT_PAGE:
+            case ACTION_NAME.LAST_PAGE:
+                handleAction(actionName, logEventNumRef.current, numEventsRef.current);
+                break;
+            case ACTION_NAME.PAGE_TOP:
+                goToPositionAndCenter(editor, {lineNumber: 1, column: 1});
+                break;
+            case ACTION_NAME.PAGE_BOTTOM: {
+                const lineCount = editor.getModel()?.getLineCount();
+                if ("undefined" === typeof lineCount) {
+                    break;
+                }
+                goToPositionAndCenter(editor, {lineNumber: lineCount, column: 1});
+                break;
+            }
+            default:
+                break;
+        }
+    }, []);
 
     /**
      * Sets `editorRef` and configures callbacks for mouse down detection.
@@ -123,9 +155,19 @@ const Editor = ({onCustomAction}: EditorProps) => {
         beginLineNumToLogEventNumRef.current = beginLineNumToLogEventNum;
     }, [beginLineNumToLogEventNum]);
 
+    // Synchronize `logEventNumRef` with `logEventNum`.
+    useEffect(() => {
+        logEventNumRef.current = logEventNum;
+    }, [logEventNum]);
+
+    // Synchronize `numEventsRef` with `numEvents`.
+    useEffect(() => {
+        numEventsRef.current = numEvents;
+    }, [numEvents]);
+
     // On `logEventNum` update, update line number in the editor.
     useEffect(() => {
-        if (null === editorRef.current || true === isMouseDownRef.current) {
+        if (null === editorRef.current || isMouseDownRef.current) {
             // Don't update the line number if the user is actively selecting text.
             return;
         }
@@ -144,15 +186,17 @@ const Editor = ({onCustomAction}: EditorProps) => {
     ]);
 
     return (
-        <MonacoInstance
-            actions={EDITOR_ACTIONS}
-            beforeTextUpdate={resetCachedPageSize}
-            lineNum={lineNum}
-            text={logData}
-            onCursorExplicitPosChange={handleCursorExplicitPosChange}
-            onCustomAction={onCustomAction}
-            onMount={handleMount}
-            onTextUpdate={restoreCachedPageSize}/>
+        <div className={"editor"}>
+            <MonacoInstance
+                actions={EDITOR_ACTIONS}
+                beforeTextUpdate={resetCachedPageSize}
+                lineNum={lineNum}
+                text={logData}
+                onCursorExplicitPosChange={handleCursorExplicitPosChange}
+                onCustomAction={handleEditorCustomAction}
+                onMount={handleMount}
+                onTextUpdate={restoreCachedPageSize}/>
+        </div>
     );
 };
 

new-log-viewer/src/components/Layout.tsx
@@ -1,164 +1,13 @@
-import React, {
-    useCallback,
-    useContext,
-    useEffect,
-    useRef,
-} from "react";
+import {CssVarsProvider} from "@mui/joy/styles";
 
-import * as monaco from "monaco-editor";
-
-import {StateContext} from "../contexts/StateContextProvider";
-import {
-    copyPermalinkToClipboard,
-    updateWindowUrlHashParams,
-    UrlContext,
-} from "../contexts/UrlContextProvider";
-import {Nullable} from "../typings/common";
-import {
-    CONFIG_KEY,
-    LOCAL_STORAGE_KEY,
-    THEME_NAME,
-} from "../typings/config";
-import {CURSOR_CODE} from "../typings/worker";
-import {ACTION_NAME} from "../utils/actions";
-import {
-    getConfig,
-    setConfig,
-} from "../utils/config";
-import {openFile} from "../utils/file";
-import {
-    getFirstItemNumInNextChunk,
-    getLastItemNumInPrevChunk,
-} from "../utils/math";
+import {CONFIG_KEY} from "../typings/config";
+import {CONFIG_DEFAULT} from "../utils/config";
 import DropFileContainer from "./DropFileContainer";
 import Editor from "./Editor";
-import {goToPositionAndCenter} from "./Editor/MonacoInstance/utils";
+import MenuBar from "./MenuBar";
+import StatusBar from "./StatusBar";
+import APP_THEME from "./theme";
 
-
-const formFields = [
-    {
-        initialValue: getConfig(CONFIG_KEY.DECODER_OPTIONS).formatString,
-        label: LOCAL_STORAGE_KEY.DECODER_OPTIONS_FORMAT_STRING,
-        name: LOCAL_STORAGE_KEY.DECODER_OPTIONS_FORMAT_STRING,
-        type: "text",
-    },
-    {
-        initialValue: getConfig(CONFIG_KEY.DECODER_OPTIONS).logLevelKey,
-        label: LOCAL_STORAGE_KEY.DECODER_OPTIONS_LOG_LEVEL_KEY,
-        name: LOCAL_STORAGE_KEY.DECODER_OPTIONS_LOG_LEVEL_KEY,
-        type: "text",
-    },
-    {
-        initialValue: getConfig(CONFIG_KEY.DECODER_OPTIONS).timestampKey,
-        label: LOCAL_STORAGE_KEY.DECODER_OPTIONS_TIMESTAMP_KEY,
-        name: LOCAL_STORAGE_KEY.DECODER_OPTIONS_TIMESTAMP_KEY,
-        type: "text",
-    },
-    {
-        initialValue: getConfig(CONFIG_KEY.THEME),
-        label: LOCAL_STORAGE_KEY.THEME,
-        name: LOCAL_STORAGE_KEY.THEME,
-        type: "text",
-    },
-    {
-        initialValue: getConfig(CONFIG_KEY.PAGE_SIZE),
-        label: LOCAL_STORAGE_KEY.PAGE_SIZE,
-        name: LOCAL_STORAGE_KEY.PAGE_SIZE,
-        type: "number",
-    },
-];
-
-/**
- * Renders a form for testing config utilities.
- *
- * @return
- */
-const ConfigForm = () => {
-    const handleConfigFormReset = (ev: React.FormEvent) => {
-        ev.preventDefault();
-        window.localStorage.clear();
-        window.location.reload();
-    };
-
-    const handleConfigFormSubmit = (ev: React.FormEvent) => {
-        ev.preventDefault();
-        const formData = new FormData(ev.target as HTMLFormElement);
-
-        const formatString = formData.get(LOCAL_STORAGE_KEY.DECODER_OPTIONS_FORMAT_STRING);
-        const logLevelKey = formData.get(LOCAL_STORAGE_KEY.DECODER_OPTIONS_LOG_LEVEL_KEY);
-        const timestampKey = formData.get(LOCAL_STORAGE_KEY.DECODER_OPTIONS_TIMESTAMP_KEY);
-        const theme = formData.get(LOCAL_STORAGE_KEY.THEME);
-        const pageSize = formData.get(LOCAL_STORAGE_KEY.PAGE_SIZE);
-        let error = null;
-        if (
-            "string" === typeof formatString &&
-            "string" === typeof logLevelKey &&
-            "string" === typeof timestampKey
-        ) {
-            error ||= setConfig({
-                key: CONFIG_KEY.DECODER_OPTIONS,
-                value: {formatString, logLevelKey, timestampKey},
-            });
-        }
-        if ("string" === typeof theme) {
-            error ||= setConfig({
-                key: CONFIG_KEY.THEME,
-                value: theme as THEME_NAME,
-            });
-        }
-        if ("string" === typeof pageSize) {
-            error ||= setConfig({
-                key: CONFIG_KEY.PAGE_SIZE,
-                value: Number(pageSize),
-            });
-        }
-        if (null !== error) {
-            // eslint-disable-next-line no-alert
-            window.alert(error);
-        } else {
-            window.location.reload();
-        }
-    };
-
-    return (
-        <form
-            onReset={handleConfigFormReset}
-            onSubmit={handleConfigFormSubmit}
-        >
-            <table>
-                <tbody>
-                    {formFields.map((field, index) => (
-                        <tr key={index}>
-                            <td>
-                                {field.label}
-                            </td>
-                            <td>
-                                <input
-                                    defaultValue={field.initialValue}
-                                    name={field.name}
-                                    size={100}
-                                    type={field.type}/>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-            <div>
-                <button type={"submit"}>Apply</button>
-                <button type={"reset"}>Clear localStorage</button>
-            </div>
-        </form>
-    );
-};
-
-/**
- * Handles `logEventNum` input value change for debugging.
- *
- * @param ev
- */
-const handleLogEventNumInputChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
-    updateWindowUrlHashParams({logEventNum: Number(ev.target.value)});
-};
 
 /**
  * Renders the major layout of the log viewer.
@@ -166,124 +15,20 @@ const handleLogEventNumInputChange = (ev: React.ChangeEvent<HTMLInputElement>) =
  * @return
  */
 const Layout = () => {
-    const {
-        fileName,
-        loadFile,
-        numEvents,
-        pageNum,
-    } = useContext(StateContext);
-    const {logEventNum} = useContext(UrlContext);
-
-    const logEventNumRef = useRef<Nullable<number>>(logEventNum);
-    const numEventsRef = useRef<Nullable<number>>(numEvents);
-
-    const handleCopyLinkButtonClick = () => {
-        copyPermalinkToClipboard({}, {logEventNum: numEvents});
-    };
-
-    const handleOpenFileButtonClick = () => {
-        openFile((file) => {
-            loadFile(file, {code: CURSOR_CODE.LAST_EVENT, args: null});
-        });
-    };
-
-    /**
-     * Handles custom actions in the editor.
-     *
-     * @param editor
-     * @param actionName
-     */
-    const handleCustomAction = useCallback((
-        editor: monaco.editor.IStandaloneCodeEditor,
-        actionName: ACTION_NAME
-    ) => {
-        const pageSize = getConfig(CONFIG_KEY.PAGE_SIZE);
-
-        switch (actionName) {
-            case ACTION_NAME.FIRST_PAGE:
-                updateWindowUrlHashParams({logEventNum: 1});
-                break;
-            case ACTION_NAME.PREV_PAGE:
-                if (null !== logEventNumRef.current) {
-                    updateWindowUrlHashParams({
-                        logEventNum: getLastItemNumInPrevChunk(logEventNumRef.current, pageSize),
-                    });
-                }
-                break;
-            case ACTION_NAME.NEXT_PAGE:
-                if (null !== logEventNumRef.current) {
-                    updateWindowUrlHashParams({
-                        logEventNum: getFirstItemNumInNextChunk(logEventNumRef.current, pageSize),
-                    });
-                }
-                break;
-            case ACTION_NAME.LAST_PAGE:
-                updateWindowUrlHashParams({logEventNum: numEventsRef.current});
-                break;
-            case ACTION_NAME.PAGE_TOP:
-                goToPositionAndCenter(editor, {lineNumber: 1, column: 1});
-                break;
-            case ACTION_NAME.PAGE_BOTTOM: {
-                const lineCount = editor.getModel()?.getLineCount();
-                if ("undefined" === typeof lineCount) {
-                    break;
-                }
-                goToPositionAndCenter(editor, {lineNumber: lineCount, column: 1});
-                break;
-            }
-            default: break;
-        }
-    }, []);
-
-    // Synchronize `logEventNumRef` with `logEventNum`.
-    useEffect(() => {
-        logEventNumRef.current = logEventNum;
-    }, [logEventNum]);
-
-    // Synchronize `numEventsRef` with `numEvents`.
-    useEffect(() => {
-        numEventsRef.current = numEvents;
-    }, [numEvents]);
-
     return (
-        <>
-            <div style={{display: "flex", flexDirection: "column", height: "100%"}}>
-                <h3>
-                    LogEventNum -
-                    {" "}
-                    <input
-                        type={"number"}
-                        value={null === logEventNum ?
-                            1 :
-                            logEventNum}
-                        onChange={handleLogEventNumInputChange}/>
-                    {" "}
-                    |
-                    PageNum -
-                    {" "}
-                    {pageNum}
-                    {" "}
-                    | FileName -
-                    {" "}
-                    {fileName}
-                </h3>
-
-                <button onClick={handleCopyLinkButtonClick}>
-                    Copy link to last log
-                </button>
-
-                <button onClick={handleOpenFileButtonClick}>
-                    Open File
-                </button>
-
-                <ConfigForm/>
-                <div style={{flexDirection: "column", flexGrow: 1}}>
-                    <DropFileContainer>
-                        <Editor onCustomAction={handleCustomAction}/>
-                    </DropFileContainer>
-                </div>
+        <CssVarsProvider
+            defaultMode={CONFIG_DEFAULT[CONFIG_KEY.THEME]}
+            modeStorageKey={CONFIG_KEY.THEME}
+            theme={APP_THEME}
+        >
+            <div className={"layout"}>
+                <MenuBar/>
+                <DropFileContainer>
+                    <Editor/>
+                </DropFileContainer>
+                <StatusBar/>
             </div>
-        </>
+        </CssVarsProvider>
     );
 };
 

new-log-viewer/src/components/MenuBar/NavigationBar.tsx
@@ -1,0 +1,70 @@
+import React, {useContext} from "react";
+
+import NavigateBefore from "@mui/icons-material/NavigateBefore";
+import NavigateNext from "@mui/icons-material/NavigateNext";
+import SkipNext from "@mui/icons-material/SkipNext";
+import SkipPrevious from "@mui/icons-material/SkipPrevious";
+
+import {StateContext} from "../../contexts/StateContextProvider";
+import {UrlContext} from "../../contexts/UrlContextProvider";
+import {
+    ACTION_NAME,
+    handleAction,
+} from "../../utils/actions";
+import PageNumInput from "./PageNumInput";
+import SmallIconButton from "./SmallIconButton";
+
+
+/**
+ * Renders a navigation bar for page switching actions.
+ *
+ * @return
+ */
+const NavigationBar = () => {
+    const {numEvents} = useContext(StateContext);
+    const {logEventNum} = useContext(UrlContext);
+
+    const handleNavButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        if (null === logEventNum) {
+            return;
+        }
+        const {actionName} = event.currentTarget.dataset as { actionName: ACTION_NAME };
+        if (Object.values(ACTION_NAME).includes(actionName)) {
+            handleAction(actionName, logEventNum, numEvents);
+        }
+    };
+
+    return (
+        <>
+            <SmallIconButton
+                data-action-name={ACTION_NAME.FIRST_PAGE}
+                onClick={handleNavButtonClick}
+            >
+                <SkipPrevious/>
+            </SmallIconButton>
+            <SmallIconButton
+                data-action-name={ACTION_NAME.PREV_PAGE}
+                onClick={handleNavButtonClick}
+            >
+                <NavigateBefore/>
+            </SmallIconButton>
+
+            <PageNumInput/>
+
+            <SmallIconButton
+                data-action-name={ACTION_NAME.NEXT_PAGE}
+                onClick={handleNavButtonClick}
+            >
+                <NavigateNext/>
+            </SmallIconButton>
+            <SmallIconButton
+                data-action-name={ACTION_NAME.LAST_PAGE}
+                onClick={handleNavButtonClick}
+            >
+                <SkipNext/>
+            </SmallIconButton>
+        </>
+    );
+};
+
+export default NavigationBar;

commit 2f653ebfd07c96efe2d3c8d92280c46911a18169

new-log-viewer: Fix lint errors introduced in #59. (#70)

new-log-viewer/src/components/MenuBar/PageNumInput.tsx
@@ -55,7 +55,8 @@ const PageNumInput = () => {
             return;
         }
         inputRef.current.style.width = "0";
-        inputRef.current.style.width = `${inputRef.current.scrollWidth + PAGE_NUM_INPUT_FIT_EXTRA_WIDTH}px`;
+        inputRef.current.style.width =
+            `${inputRef.current.scrollWidth + PAGE_NUM_INPUT_FIT_EXTRA_WIDTH}px`;
     };
 
     const handleInputChange = () => {

new-log-viewer/src/utils/config.ts
@@ -31,6 +31,7 @@ const CONFIG_DEFAULT: ConfigMap = Object.freeze({
  * @param props.key
  * @param props.value
  * @return `null` if the value is valid, or an error message otherwise.
+ * @throws {Error} If the config item cannot be managed by these config utilities.
  */
 const testConfig = ({key, value}: ConfigUpdate): Nullable<string> => {
     let result = null;
@@ -41,7 +42,7 @@ const testConfig = ({key, value}: ConfigUpdate): Nullable<string> => {
             }
             break;
         case CONFIG_KEY.THEME:
-            throw new Error(`${key} should be handled by JoyUI instead.`);
+            throw new Error(`"${key}" cannot be managed using these utilities.`);
         case CONFIG_KEY.PAGE_SIZE:
             if (0 >= value || MAX_PAGE_SIZE < value) {
                 result = `Page size must be greater than 0 and less than ${MAX_PAGE_SIZE + 1}.`;
@@ -61,6 +62,7 @@ const testConfig = ({key, value}: ConfigUpdate): Nullable<string> => {
  * @param props.key
  * @param props.value
  * @return `null` if the update succeeds, or an error message otherwise.
+ * @throws {Error} If the config item cannot be managed by these config utilities.
  */
 const setConfig = ({key, value}: ConfigUpdate): Nullable<string> => {
     const error = testConfig({key, value} as ConfigUpdate);
@@ -86,7 +88,7 @@ const setConfig = ({key, value}: ConfigUpdate): Nullable<string> => {
             );
             break;
         case CONFIG_KEY.THEME:
-            throw new Error(`${key} should be handled by JoyUI instead.`);
+            throw new Error(`"${key}" cannot be managed using these utilities.`);
         case CONFIG_KEY.PAGE_SIZE:
             window.localStorage.setItem(LOCAL_STORAGE_KEY.PAGE_SIZE, value.toString());
             break;
@@ -101,6 +103,7 @@ const setConfig = ({key, value}: ConfigUpdate): Nullable<string> => {
  *
  * @param key
  * @return The value.
+ * @throws {Error} If the config item cannot be managed by these config utilities.
  */
 const getConfig = <T extends CONFIG_KEY>(key: T): ConfigMap[T] => {
     let value = null;
@@ -121,7 +124,7 @@ const getConfig = <T extends CONFIG_KEY>(key: T): ConfigMap[T] => {
             } as DecoderOptionsType;
             break;
         case CONFIG_KEY.THEME:
-            throw new Error(`${key} should be handled by JoyUI instead.`);
+            throw new Error(`"${key}" cannot be managed using these utilities.`);
         case CONFIG_KEY.PAGE_SIZE:
             value = window.localStorage.getItem(LOCAL_STORAGE_KEY.PAGE_SIZE);
             break;