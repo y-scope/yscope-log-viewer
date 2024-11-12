30481f99ccbe6e653a654c763f783879169160ef
Henry <50559854+Henry8192@users.noreply.github.com>
2024-11-12 16:46:42 -0500

    fix lint

--- src/services/LogFileManager/index.ts
+++ src/services/LogFileManager/index.ts
@@ -293,7 +293,8 @@ class LogFileManager {
         this.#queryId++;
         this.#queryCount = 0;
 
-        // Send an empty query result with 0 progress to the render to init the results variable because there could be results sent by the last task before `startQuery()` runs.
+        // Send an empty query result with 0 progress to the render to init the results variable
+        // because there could be results sent by previous task before `startQuery()` runs.
         this.#onQueryResults(0, new Map());
 
         // If the query string is empty, or there are no logs, return
