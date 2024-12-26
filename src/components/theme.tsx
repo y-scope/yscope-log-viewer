d788ac0d18e0a63fe240d94bb27b44c0d2087698
Junhao Liao <junhao@junhao.ca>
2024-12-26 02:09:18 -0500

    Fix prop order for JoyTooltip defaultProps.

--- src/components/theme.tsx
+++ src/components/theme.tsx
@@ -57,8 +57,8 @@ const APP_THEME = extendTheme({
         },
         JoyTooltip: {
             defaultProps: {
-                variant: "outlined",
                 arrow: true,
+                variant: "outlined",
             },
         },
     },
