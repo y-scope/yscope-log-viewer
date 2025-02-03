d5e189cd1555d6ed82ea68257e7fbbf8f46d80a8
Junhao Liao <junhao@junhao.ca>
2025-02-03 02:22:09 -0500

    Refactor timestamp handling to use bigint type

--- src/services/decoders/JsonlDecoder/index.ts
+++ src/services/decoders/JsonlDecoder/index.ts
@@ -214,7 +214,7 @@ class JsonlDecoder implements Decoder {
      * @return The decoded log event.
      */
     #decodeLogEvent = (logEventIdx: number): DecodeResult => {
-        let timestamp: number;
+        let timestamp: bigint;
         let message: string;
         let logLevel: LOG_LEVEL;
 
@@ -231,7 +231,7 @@ class JsonlDecoder implements Decoder {
             const logEvent = this.#logEvents[logEventIdx] as LogEvent;
             logLevel = logEvent.level;
             message = this.#formatter.formatLogEvent(logEvent);
-            timestamp = logEvent.timestamp.valueOf();
+            timestamp = BigInt(logEvent.timestamp.valueOf());
         }
 
         return [
