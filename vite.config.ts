da2ecd7a225e4232df75bcfb1c894357761f19c8
Junhao Liao <junhao@junhao.ca>
2025-02-13 05:33:27 +0800

    Set relative base path in Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from "vite";
 // https://vite.dev/config/
 export default defineConfig({
     appType: "mpa",
+    base: "./",
     build: {
         rollupOptions: {
             output: {
