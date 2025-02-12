231c924f387a78bd37428c34f07021fc980d4355
Junhao Liao <junhao@junhao.ca>
2025-02-13 01:27:22 +0800

    Remove onProgress callback from getUint8ArrayFrom()

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,10 +1,5 @@
-import axios, {
-    AxiosError,
-    AxiosProgressEvent,
-} from "axios";
+import axios, {AxiosError} from "axios";
 
-
-type ProgressCallback = (numBytesLoaded:number, numBytesTotal:number) => void;
 
 /**
  * Converts an Axios error into a custom Error object.
@@ -35,41 +30,20 @@ const convertAxiosError = (e: AxiosError): Error => {
     );
 };
 
-
-/**
- * Normalizes the total size of a download / upload event, and calls the provided onProgress
- * callback with loaded and total sizes.
- *
- * @param onProgress
- * @return The handler that wraps `onProgress`.
- */
-const normalizeTotalSize = (onProgress: ProgressCallback) => ({
-    loaded,
-    total,
-}: AxiosProgressEvent) => {
-    if ("undefined" === typeof total || isNaN(total)) {
-        total = loaded;
-    }
-    onProgress(loaded, total);
-};
-
 /**
  * Downloads (bypassing any caching) a file as a Uint8Array.
  *
  * @param fileUrl
- * @param [onProgress]
  * @return The file's content.
  * @throws {Error} if the download fails.
  */
 const getUint8ArrayFrom = async (
     fileUrl: string,
-    onProgress: ProgressCallback = () => null
 )
 : Promise<Uint8Array> => {
     try {
         const {data} = await axios.get<ArrayBuffer>(fileUrl, {
             responseType: "arraybuffer",
-            onDownloadProgress: normalizeTotalSize(onProgress),
             headers: {
                 "Cache-Control": "no-cache",
                 "Pragma": "no-cache",
