ce20db7f078b370546e968839f771e7617c56ef7
Junhao Liao <junhao@junhao.ca>
2025-02-14 21:11:58 +0800

    Remove monaco bootstrap import in main.tsx.

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import {createRoot} from "react-dom/client";
 import App from "./App";
 
 import "./main.css";
-import "./components/Editor/MonacoInstance/bootstrap";
 
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
