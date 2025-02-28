691a911094d68742a397605588616b016dc684c8
Henry8192 <50559854+Henry8192@users.noreply.github.com>
2025-02-28 16:22:23 -0500

    startQuery should first check the corner case before incrementing queryId

--- src/services/LogFileManager/index.ts
+++ src/services/LogFileManager/index.ts
@@ -296,17 +296,17 @@ class LogFileManager {
      * @throws {SyntaxError} if the query regex string is invalid.
      */
     startQuery ({queryString, queryIsRegex, queryIsCaseSensitive}: QueryArgs): void {
+        // If the query string is empty, or there are no logs, return
+        if ("" === queryString || 0 === this.#numEvents) {
+            return;
+        }
+
         this.#queryId++;
         this.#queryCount = 0;
 
         // Send an empty query result with 0 progress to the render to init the results variable
         // because there could be results sent by previous task before `startQuery()` runs.
         this.#onQueryResults(0, new Map());
-
-        // If the query string is empty, or there are no logs, return
-        if ("" === queryString || 0 === this.#numEvents) {
-            return;
-        }
 
         // Construct query RegExp
         const regexPattern = queryIsRegex ?
