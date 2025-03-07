d0d071b84863367ea562d1424931e82519402ef6
Henry8192 <50559854+Henry8192@users.noreply.github.com>
2025-03-07 11:27:30 -0500

    Update src/components/Editor/MonacoInstance/language.ts
    Co-authored-by: Junhao Liao <junhao@junhao.ca>

--- src/components/Editor/MonacoInstance/language.ts
+++ src/components/Editor/MonacoInstance/language.ts
@@ -21,7 +21,7 @@ const setupCustomLogLanguage = () => {
                     TOKEN_NAME.CUSTOM_INFO,
                 ],
                 [
-                    /\b(WARN|WARNING)\b/,
+                    "WARNING|WARN",
                     TOKEN_NAME.CUSTOM_WARN,
                 ],
                 [
