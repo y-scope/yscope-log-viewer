2b06efdab8754b8b950f2992f5fba3313bf2db8b
Henry8192 <50559854+Henry8192@users.noreply.github.com>
2025-04-14 09:22:02 -0400

    fix not clearing previous query results

--- src/contexts/states/mainWorkerStore.ts
+++ src/contexts/states/mainWorkerStore.ts
@@ -56,7 +56,11 @@ const handleMainWorkerResp = (ev: MessageEvent<MainWorkerRespMessage>) => {
             setUiState(UI_STATE.READY);
             break;
         case WORKER_RESP_CODE.QUERY_RESULT: {
-            const {setQueryProgress, mergeQueryResults} = useQueryStore.getState();
+            const {clearQueryResults, setQueryProgress, mergeQueryResults} = useQueryStore.getState();
+            if (0 === args.progress) {
+                clearQueryResults();
+                break;
+            }
             setQueryProgress(args.progress);
             mergeQueryResults(args.results);
             break;
