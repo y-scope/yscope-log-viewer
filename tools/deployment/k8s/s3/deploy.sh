b59398175c896794567220978f82f886cc89a01e
Jack Luo <jack.luo@mail.utoronto.ca>
2025-05-27 13:57:40 -0400

    Addressed kirkrodrigues's code review concerns.

--- tools/deployment/k8s/s3/deploy.sh
+++ tools/deployment/k8s/s3/deploy.sh
@@ -1,84 +1,92 @@
 #!/usr/bin/env bash
-set -eo pipefail
+set -e
+set -o pipefail
+set -u
 
-# AWS CLI Container Image Script
-# Ensure AWS CLI environment setup:
-# - Use official AWS CLI container image (amazon/aws-cli) and ensure curl, tar, gzip, and jq is preinstalled.
-# - Configure AWS credentials using environment variables through various options:
-#   - Put credential files under $HOME/.aws/credentials
-#   - Set AWS_CONFIG_FILE for custom configuration file path.
-#   - Set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY directly.
-#   - Refer to AWS CLI docs for more authentication options.
+# This script is designed to work with AWS CLI Container image, but may also be useful for other use-cases.
+# It is the user's responsibility to:
+# 1. Ensure curl, tar, gzip, and jq commands are preinstalled
+# 2. Ensure AWS CLI environment is pre-configured with authentication if necessary through various options:
+#   a. Put credentials file under $HOME/.aws/credentials
+#   b. Set AWS_CONFIG_FILE pointing to a custom credentials file path
+#   c. Set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY directly
+#   d. Refer to AWS CLI docs for more authentication options.
 
-# Expected Environment Variables:
-# - AWS_ENDPOINT_URL: Example - "http://minio:9091"
-# - LOG_VIEWER_BUCKET: Example - "log-viewer"
-# - TAG_NAME: Example - "latest" (latest stable release), A GitHub release tag, or <empty> (latest prerelease)
-
-LOG() {
-    echo "$(date -u +"%Y-%m-%dT%H:%M:%SZ") $1" >&2
+# Function to emit a log message to stderr with auto-generated ISO timestamp, verbosity and message
+#
+# @param $1: Verbosity level string
+# $param $2: Message to be logged
+log() {
+    local verbosity=$1
+    local message=$2
+    echo "$(date -u +"%Y-%m-%dT%H:%M:%SZ") [$verbosity] $message" >&2
 }
 
+# Function to wait until S3 endpoint is available by listing the available buckets from the S3 endpoint.
+# If the listing operation succeeds, the function returns immediately.
+# Otherwise, it retries a maximum of 10 times with a delay of 6 seconds between each retry.
 wait_for_s3_availability() {
-    local max_retries=10
-    local retry_delay=6
+    local MAX_RETRIES=10
+    local RETRY_DELAY_IN_SECS=6
 
-    local retries=0
-    while [ $retries -lt $max_retries ]; do
-        if ! aws s3 ls --endpoint-url "$AWS_ENDPOINT_URL" > /dev/null 2>&1; then
-            LOG "[INFO] S3 API endpoint did not return successfully. Retrying in $retry_delay seconds ..."
-            retries=$((retries+1))
-            sleep $retry_delay
-        else
-            break
+    for ((retries = 0; retries < $MAX_RETRIES; retries++)); do
+        if aws s3 ls --endpoint-url "$AWS_ENDPOINT_URL"; then
+          return
         fi
+        log "WARN" "S3 API endpoint didn't return successfully. Retrying in $RETRY_DELAY_IN_SECS seconds ..."
+        sleep $RETRY_DELAY_IN_SECS
     done
 
-    if [ $retries -eq $max_retries ]; then
-        LOG "[ERROR] Maximum retries reached. S3 API endpoint ${AWS_ENDPOINT_URL} didn't respond"
+    if [ $retries -eq $MAX_RETRIES ]; then
+        log "ERROR" "Maximum retries reached. S3 API endpoint ${AWS_ENDPOINT_URL} didn't respond."
         exit 1
     fi
 }
 
-# Validate that mandatory environment variables are set
-for var in "AWS_ENDPOINT_URL" "LOG_VIEWER_BUCKET"; do
+# Validate required environment variables and populate optional one if not provided
+readonly REQUIRED_ENV_VARS=(
+  # Example: "http://minio:9091"
+  "AWS_ENDPOINT_URL"
+
+  # Example: "log-viewer"
+  "LOG_VIEWER_BUCKET"
+)
+for var in "${REQUIRED_ENV_VARS[@]}"; do
     if [ -z "${!var}" ]; then
-        LOG "[ERROR] $var environment variable must be specified"
+        log "ERROR" "$var environment variable must be set."
         exit 1
     fi
 done
-
-# Fetch RELEASE_TARBALL_URL via GitHub API and download release
+readonly GITHUB_RELEASES_API_ENDPOINT="https://api.github.com/repos/y-scope/yscope-log-viewer/releases"
 if [ -z "$TAG_NAME" ]; then
     # If not defined, use latest prerelease
-    RELEASE_TARBALL_URL=$(curl --silent --show-error \
-      "https://api.github.com/repos/y-scope/yscope-log-viewer/releases" | \
-      jq -r 'map(select(.prerelease)) | first | .assets[0].browser_download_url')
+    RELEASE_TARBALL_URL=$(curl --silent --show-error $GITHUB_RELEASES_API_ENDPOINT \
+        | jq --raw-output 'map(select(.prerelease)) | first | .assets[0].browser_download_url')
 else
-    RELEASE_TARBALL_URL=$(curl --silent --show-error \
-      "https://api.github.com/repos/y-scope/yscope-log-viewer/releases/${TAG_NAME}" | \
-      jq -r '.assets[0].browser_download_url')
+    RELEASE_TARBALL_URL=$(curl --silent --show-error "${GITHUB_RELEASES_API_ENDPOINT}/${TAG_NAME}" \
+        | jq --raw-output '.assets[0].browser_download_url')
 fi
 if [ -z "$RELEASE_TARBALL_URL" ]; then
-  LOG "[ERROR] Could not automatically resolve release tarball URL"
-  exit 1
+    log "ERROR" "Cannot resolve release tarball URL."
+    exit 1
 fi
 
 # Wait until S3 endpoint is available
-LOG "[INFO] Waiting until s3://${LOG_VIEWER_BUCKET} endpoint becomes available ..."
+log "INFO" "Waiting until s3://${LOG_VIEWER_BUCKET} endpoint becomes available ..."
 wait_for_s3_availability
 
 # Create log-viewer bucket if not already exist
-LOG "[INFO] Creating s3://${LOG_VIEWER_BUCKET} bucket"
+log "INFO" "Creating s3://${LOG_VIEWER_BUCKET} bucket."
 if ! aws s3api head-bucket --endpoint-url "$AWS_ENDPOINT_URL" --bucket "$LOG_VIEWER_BUCKET" 2>/dev/null; then
     aws s3api create-bucket --endpoint-url "$AWS_ENDPOINT_URL" --bucket "$LOG_VIEWER_BUCKET"
 else
-    LOG "[WARN] Bucket s3://${LOG_VIEWER_BUCKET} already exists"
+    log "WARN" "Bucket s3://${LOG_VIEWER_BUCKET} already exist."
 fi
 
 # Define and apply the Bucket Policy for public read access
-LOG "[INFO] Applying public read access policy to s3://${LOG_VIEWER_BUCKET}"
-POLICY=$(cat <<EOP
+log "INFO" "Applying public read access policy to s3://${LOG_VIEWER_BUCKET}"
+readonly POLICY=$(
+    cat <<EOP
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -93,18 +101,17 @@ POLICY=$(cat <<EOP
 EOP
 )
 if ! aws s3api put-bucket-policy \
-  --endpoint-url "${AWS_ENDPOINT_URL}" --bucket "${LOG_VIEWER_BUCKET}" --policy "$POLICY"; then
-    LOG "[ERROR] Failed to set bucket policy for s3://${LOG_VIEWER_BUCKET}"
+    --endpoint-url "${AWS_ENDPOINT_URL}" --bucket "${LOG_VIEWER_BUCKET}" --policy "$POLICY"; then
+    LOG "ERROR" "Failed to set bucket policy for s3://${LOG_VIEWER_BUCKET}"
     exit 1
 fi
 
 # Download and decompress release tarball to a temp directory
-LOG "[INFO] Downloading ${RELEASE_TARBALL_URL}"
-DECOMPRESSED_ASSETS_DIRECTORY="${DECOMPRESSED_ASSETS_DIRECTORY:-/tmp/yscope-log-viewer}"
-mkdir -p "$DECOMPRESSED_ASSETS_DIRECTORY"
-if ! curl --silent --show-error --location "$RELEASE_TARBALL_URL" | \
-  tar --strip-components 1 -xz -C "$DECOMPRESSED_ASSETS_DIRECTORY"; then
-    LOG "[ERROR] Failed to download and extract the release tarball from ${RELEASE_TARBALL_URL}"
+LOG "INFO" "Downloading ${RELEASE_TARBALL_URL}"
+readonly DECOMPRESSED_ASSETS_DIRECTORY=$(mktemp -d)
+if ! curl --silent --show-error --location "$RELEASE_TARBALL_URL" \
+    | tar --strip-components 1 -xz -C "$DECOMPRESSED_ASSETS_DIRECTORY"; then
+    LOG "ERROR" "Failed to download and extract the release tarball from ${RELEASE_TARBALL_URL}"
     exit 1
 fi
 
@@ -112,11 +119,10 @@ fi
 # Note that uploads can fail with invalid/unknown checksum sent error.
 # This typically occurs with old MinIO. If this happens, update to release after late 2024.
 # See this GitHub issue for details: https://github.com/minio/minio/pull/19680
-LOG "[INFO] Uploading yscope-log-viewer assets to object store"
-aws s3 cp "$DECOMPRESSED_ASSETS_DIRECTORY" "s3://${LOG_VIEWER_BUCKET}/" \
-    --recursive --endpoint-url "$AWS_ENDPOINT_URL"
+LOG "INFO" "Uploading yscope-log-viewer assets to object store."
+aws s3 cp "$DECOMPRESSED_ASSETS_DIRECTORY" "s3://${LOG_VIEWER_BUCKET}/" --recursive --endpoint-url "$AWS_ENDPOINT_URL"
 
-LOG "[INFO] Deployment completed successfully"
+LOG "INFO" "Deployment completed successfully"
 
 # Print out a prompt message with a box around it
 prompt_message="yscope-log-viewer is now available at ${AWS_ENDPOINT_URL}/${LOG_VIEWER_BUCKET}/index.html"
@@ -125,6 +131,6 @@ total_length=$((message_length + 6))
 printf "\n\n"
 printf "+%${total_length}s+\n" | tr ' ' "-"
 printf "|%${total_length}s|\n"
-printf "|   $prompt_message   |\n"
+printf "|   %s   |\n" "$prompt_message"
 printf "|%${total_length}s|\n"
 printf "+%${total_length}s+\n" | tr ' ' "-"
