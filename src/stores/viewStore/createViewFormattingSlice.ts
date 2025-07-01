0f646867f316f10f520a0dfb6d5bb75b69affa0d
hoophalab <200652805+hoophalab@users.noreply.github.com>
2025-07-01 16:48:18 -0400

    feat: Add timezone select UI to the status bar.
    tmp

--- src/stores/viewStore/createViewFormattingSlice.ts
+++ src/stores/viewStore/createViewFormattingSlice.ts
@@ -1,11 +1,13 @@
 import {StateCreator} from "zustand";
 
+import {DEFAULT_TIMEZONE_NAME} from "../../typings/date";
 import {UI_STATE} from "../../typings/states";
 import {
     CURSOR_CODE,
     CursorType,
 } from "../../typings/worker";
 import useLogFileManagerStore from "../logFileManagerProxyStore";
+import useLogFileStore from "../logFileStore";
 import {handleErrorWithNotification} from "../notificationStore";
 import useUiStore from "../uiStore";
 import {VIEW_EVENT_DEFAULT} from "./createViewEventSlice";
@@ -18,6 +20,7 @@ import {
 
 const VIEW_FORMATTING_DEFAULT: ViewFormattingValues = {
     isPrettified: false,
+    timezoneName: DEFAULT_TIMEZONE_NAME,
 };
 
 /**
@@ -56,6 +59,48 @@ const createViewFormattingSlice: StateCreator<
             const pageData = await logFileManagerProxy.loadPage(cursor, newIsPrettified);
 
             const {updatePageData} = get();
+            updatePageData(pageData);
+        })().catch(handleErrorWithNotification);
+    },
+    updateTimezoneName: (newTimezoneName: string) => {
+        if ("" === newTimezoneName) {
+            newTimezoneName = DEFAULT_TIMEZONE_NAME;
+        }
+
+        const {numEvents} = useLogFileStore.getState();
+        if (0 === numEvents) {
+            return;
+        }
+
+        const {timezoneName} = get();
+        if (newTimezoneName === timezoneName) {
+            return;
+        }
+
+        const {setUiState} = useUiStore.getState();
+        setUiState(UI_STATE.FAST_LOADING);
+
+        set({timezoneName: newTimezoneName});
+
+        const {logEventNum} = get();
+        let cursor: CursorType = {code: CURSOR_CODE.LAST_EVENT, args: null};
+        if (VIEW_EVENT_DEFAULT.logEventNum !== logEventNum) {
+            cursor = {
+                code: CURSOR_CODE.EVENT_NUM,
+                args: {eventNum: logEventNum},
+            };
+        }
+
+        (async () => {
+            const {logFileManagerProxy} = useLogFileManagerStore.getState();
+            const {isPrettified, updatePageData} = get();
+
+            // await logFileManagerProxy.setTimezone(newTimezoneName);
+            const pageData = await logFileManagerProxy.loadPage(
+                cursor,
+                isPrettified,
+            );
+
             updatePageData(pageData);
         })().catch(handleErrorWithNotification);
     },
