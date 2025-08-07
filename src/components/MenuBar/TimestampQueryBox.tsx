45a8accef38089ec2fd292b50d78bdc124f0857e
Henry8192 <50559854+Henry8192@users.noreply.github.com>
2025-08-07 17:31:42 -0400

    track joyUI theme instead of system theme

--- src/components/MenuBar/TimestampQueryBox.tsx
+++ src/components/MenuBar/TimestampQueryBox.tsx
@@ -1,8 +1,12 @@
-import {Box} from "@mui/joy";
+import {
+    Box,
+    useColorScheme,
+} from "@mui/joy";
 
 import SearchIcon from "@mui/icons-material/Search";
 
 import useUiStore from "../../stores/uiStore";
+import {THEME_NAME} from "../../typings/config";
 import {UI_ELEMENT} from "../../typings/states";
 import {isDisabled} from "../../utils/states";
 import {updateWindowUrlHashParams} from "../../utils/url";
@@ -35,15 +39,18 @@ const handleTimestampQuery = (datetime: string) => {
  */
 const TimestampQueryBox = () => {
     const uiState = useUiStore((state) => state.uiState);
+    const {mode, systemMode} = useColorScheme();
     return (
         <Box className={"timestamp-query-box"}>
             <input
-                className={"timestamp-query-box-input"}
                 disabled={isDisabled(uiState, UI_ELEMENT.NAVIGATION_BAR)}
                 id={"timestamp-query-box-input"}
                 step={"0.1"}
                 title={"Timestamp to seek to in UTC"}
                 type={"datetime-local"}
+                className={`timestamp-query-box-input ${(("system" === mode) ?
+                    systemMode :
+                    mode) ?? THEME_NAME.DARK}`}
                 defaultValue={new Date().toISOString()
                     .slice(0, -1)}
                 onKeyDown={(e) => {
