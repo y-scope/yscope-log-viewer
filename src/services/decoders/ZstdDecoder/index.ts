928f5f7a98ef0d7f36849bf71a2705e7d45832ea
Henry8192 <50559854+Henry8192@users.noreply.github.com>
2025-08-26 12:36:13 -0400

    bump zstddec

--- src/services/decoders/ZstdDecoder/index.ts
+++ src/services/decoders/ZstdDecoder/index.ts
@@ -1,13 +1,13 @@
-import {ZSTDDecoder} from "zstddec";
+import {ZSTDDecoder} from "zstddec/stream";
 
 import PlainTextDecoder from "../PlainTextDecoder";
 
 
 class ZstdDecoder extends PlainTextDecoder {
     static override async create (dataArray: Uint8Array) {
-        const decoder = new ZSTDDecoder();
-        await decoder.init();
-        const decompressedDataArray = decoder.decode(dataArray);
+        const decompressor = new ZSTDDecoder();
+        await decompressor.init();
+        const decompressedDataArray = decompressor.decode(dataArray);
         return new PlainTextDecoder(decompressedDataArray);
     }
 }
